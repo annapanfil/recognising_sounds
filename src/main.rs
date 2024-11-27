137bef57069dc1c8e8f31836be1faebf71e93f5e
Anna Panfil <134panka@gmail.com>
2024-11-27 15:49:13 +0100

    feat: read audio data

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use hound;
+
 fn main() {
-    println!("Hello, world!");
+    let file_path = "./data/ESC-50-master/audio/1-137-A-32.wav";
+
+    // Otwieramy plik WAV
+    let reader = hound::WavReader::open(file_path).expect("Failed to open WAV file");
+    let spec = reader.spec();
+
+     // Odczyt próbek i mapowanie ich do wektora
+    let samples: Vec<i16> = reader
+     .into_samples::<i16>() // Zmieniamy próbki na i16
+     .map(|s| s.expect("Error reading sample")) // Wyrzucenie błędu, jeśli wystąpi
+     .collect();
+
+    println!("Loaded {} samples with sample rate {}.", samples.len(), spec.sample_rate);
 }
